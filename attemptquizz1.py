8b78448d3548e2c2ca42033a47473df38e35937a
fearona179 <fearona@stu.otc.school.nz>
2026-04-22 15:18:37 +1200

    day 1 attempt 1

---
+++ attemptquizz1.py
@@ -0,0 +1,43 @@
+correctanswers = []
+wronganswers = []
+print("This is a quiz abopuit chris wood, the questions will be multi choice sop answer 1,2,3,4")
+print("How old is Chris Wood? ")
+print("1. 42")
+print("2. 28")
+print("3. 34")
+print("4. 37")
+ans = input(" What is your answer 1,2,3 or 4: ")
+if ans == 3:
+    correctanswers.append(ans)
+else:
+    wronganswers.append(ans)
+print("Where was Chris Wood born?")
+print("1. Auckland")
+print("2. Wellington")
+print("3. Christchurch")
+print("4. Dunedin")
+ans = input("What is your answer 1,2,3 or 4: ")
+if ans == 1:
+    correctanswers.append(ans)
+else:
+    wronganswers.append(ans)
+print("What position does Chris Wood play?")
+print("1. Fallback")
+print("2. Striker")
+print("3. Center Back")
+print("4. Winger")
+ans = input("What is your answer 1,2,3 or 4: ")
+if ans ==2:
+    correctanswers.append(ans)
+else:
+    wronganswers.append(ans)
+print("How many goals has he scored in his proffessional career?")
+print("1. 150")
+print("2. 135")
+print("3. 80")
+print("4. 168")
+ans = input("What is your answer 1,2,3 or 4: ")
+if ans ==2:
+    correctanswers.append(ans)
+else:
+    wronganswers.append(ans)
---
+++ quiz.py
@@ -0,0 +1,11 @@
+counter = 0
+questions = ["How old is he?", "How many goals has he scored?", "What team does he play for now?", "What was the first club he played for?", "Where was he born?"
+, "What position does he play?", "What was his best season (most g/a)?", "How many years has he played professional football for?"]
+print("This is a quiz abopuit chris wood, the questions will be multi choice so answer 1,2,3,4")
+answersq1 = ["1. 42", "2. 28", "3. 34", "4. 37"]
+for i in questions:
+    print(questions[counter])
+    for i in answersq1:
+     print(i)
+     ans == input
+    counter = counter + 1
